42496be855e484350d6336ece0f48f62c8a42ec6
smaynez <Cadisa@amher.com.mx>
2016-08-04 18:14:03 -0600

    Vista de preregistro en proceso.

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,10 @@ router.get('/signin', function(req, res) {
    res.render('sign-in', {title:'SGPL | Iniciar sesión'});
 });
 
+router.get('/signup', function(req, res) {
+    res.render('sign-up', {title: 'SGPL | Pre Registro de Usuario'});
+});
+
 router.get('/dashboard', function(req, res) {
    res.render('dashboard/dashboard', {title:'SGPL | Dashboard'});
 });
