f227b19e92c20e9eacf0000faa99eb822a5596ed
JLRD <jlrd@localhost.localdomain>
2016-08-16 00:11:41 -0600

    Se cambian template de administración.

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+//var passport = require('passport');
 
 router.get('/', function(req, res) {
    res.render("index", {title:'SGPL | Inicio'});
@@ -11,12 +11,12 @@ router.get('/signin', function(req, res) {
 });
 
 router.get('/signup', function(req, res) {
-    res.render('sign-up', {title: 'SGPL | Pre Registro de Usuario'});
+  res.render('sign-up', {title: 'SGPL | Pre Registro de Usuario'});
 });
 
 router.get('/dashboard', function(req, res) {
    res.render('dashboard/dashboard', {title:'SGPL | Dashboard'});
-});
+})
 
 router.get('/logout', function(req, res) {
   req.logout();
